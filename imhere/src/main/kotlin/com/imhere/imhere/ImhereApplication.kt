dbda0895fb7aba555de7673e186071dfef75b7fc
Kong-hana01 <gksqlsl11@khu.ac.kr>
2024-01-25 15:57:14 +0900

    chore: 프로젝트 세팅

---
+++ imhere/src/main/kotlin/com/imhere/imhere/ImhereApplication.kt
@@ -0,0 +1,11 @@
+package com.imhere.imhere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImhereApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImhereApplication>(*args)
+}
