82e9a0ce5ccca65a422aa284e8dac6f113f2243b
echo724 <echo.backend@gmail.com>
2024-01-31 19:45:09 +0900

    test: Group 테스트 추가

--- imhere/build.gradle.kts
+++ imhere/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
 }
 
 tasks.withType<KotlinCompile> {
