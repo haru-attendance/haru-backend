428526fa8f13e0deb6929d34486eb74d17c675e6
echo724 <echo.backend@gmail.com>
2024-01-31 20:13:28 +0900

    refactor: Attendance와 Group 직접 참조에서 간접 참조로 변경
    작년 수업에서 생각해보니 객체 생성 사이클이 같거나, 도메인 제약이 같을 경우에만 묶으라고 들었는데, 생각해보니 이 둘은 분리되어야할 것 같다는 생각이 들어 id 간접참조로 변경

--- imhere/src/main/kotlin/com/imhere/imhere/model/Attendance.kt
+++ imhere/src/main/kotlin/com/imhere/imhere/model/Attendance.kt
@@ -1,16 +1,13 @@
 package com.imhere.imhere.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import java.time.LocalDate
 
 @Entity
 class Attendance(
     @Column(nullable = false)
     val date: LocalDate = LocalDate.now(),
-    @ManyToOne(cascade = [CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE], fetch = FetchType.EAGER)
-    @JoinColumn(
-        name = "group_id", nullable = false, updatable = false,
-        foreignKey = ForeignKey(name = "fk_attendance_group_id_ref_group_id")
-    )
-    val group: Group,
+    @Column(nullable = false)
+    val groupId: Long,
 ) : BaseEntity()
