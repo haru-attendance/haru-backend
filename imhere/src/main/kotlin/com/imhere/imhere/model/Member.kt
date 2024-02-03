7a3d36e2c4369ae24b085befbe325ded98641e3e
kong-hana01 <79015120+kong-hana01@users.noreply.github.com>
2024-02-03 19:03:50 +0900

    feat: User, Member 엔티티 생성 (#5)
    * feat: User 객체 구현
    
    * feat: User 객체가 BaseEntity를 상속받도록 변경
    
    * feat: Member 객체 구현
    
    * feat: BaseEntity 내에 isDeleted 프로퍼티 추가

---
+++ imhere/src/main/kotlin/com/imhere/imhere/model/Member.kt
@@ -0,0 +1,18 @@
+package com.imhere.imhere.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+
+@Entity
+class Member(
+        var userId: Long?,
+        @Column(nullable = false)
+        val groupId: Long,
+        @Column(nullable = false)
+        var subGroupId: Long,
+        @Column(nullable = false)
+        var name: String,
+        @Column(nullable = false)
+        var role: Role
+) : BaseEntity() {
+}
---
+++ imhere/src/main/kotlin/com/imhere/imhere/model/User.kt
@@ -0,0 +1,15 @@
+package com.imhere.imhere.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+
+@Entity(name = "users")
+class User(
+        @Column(nullable = false, length = 30)
+        val name: String = "",
+        @Column(nullable = false)
+        val username: String = "",
+        @Column(nullable = false)
+        val password: String = ""
+) : BaseEntity() {
+}
