dd5b86bfcc56e64a7e67685d69a3a6238432d4ff
echo724 <echo.backend@gmail.com>
2024-02-03 18:35:40 +0900

    refactor: AttendanceMember의 attendance 필드 직접 참조로 변경

--- imhere/src/main/kotlin/com/imhere/imhere/model/AttendanceMember.kt
+++ imhere/src/main/kotlin/com/imhere/imhere/model/AttendanceMember.kt
@@ -1,14 +1,16 @@
 package com.imhere.imhere.model
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
 
 @Entity
 class AttendanceMember(
     @Column(nullable = false)
     val memberId: Long,
-    @Column(nullable = false)
-    val attendanceId: Long,
+    @ManyToOne(cascade = [CascadeType.ALL], optional = false)
+    val attendance: Attendance,
     @Column(nullable = false)
     val isAttended: Boolean,
 ) : BaseEntity()
