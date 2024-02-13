07c4a646e8aba36389c729470035f1f31c8d9beb
Kong-hana01 <gksqlsl11@khu.ac.kr>
2024-02-13 22:17:23 +0900

    feat: club, member, unit의 이름을 제한하는 예외 생성

--- src/main/kotlin/com/haru/attendance/exception/MemberServiceException.kt
+++ src/main/kotlin/com/haru/attendance/exception/MemberServiceException.kt
@@ -9,11 +9,21 @@ abstract class MemberServiceException(
 
     class NonMemberException(
             memberId: Long
-    ) : MemberServiceException(prefixMessage + memberId, httpStatus) {
+    ) : MemberServiceException(PREFIX_MESSAGE + memberId, httpStatus) {
 
         companion object {
+            const val PREFIX_MESSAGE = "멤버의 ID가 잘못되었습니다. 현재 멤버의 ID: "
             val httpStatus = HttpStatus.UNAUTHORIZED
-            val prefixMessage = "멤버의 id가 잘못되었습니다. 현재 memberId: "
+        }
+    }
+
+    class MemberNameException(
+            name: String
+    ) : MemberServiceException(PREFIX_MESSAGE + name, httpStatus) {
+
+        companion object {
+            const val PREFIX_MESSAGE = "멤버의 이름은 30자를 넘길 수 없습니다. 현재 멤버 이름: "
+            val httpStatus = HttpStatus.BAD_REQUEST
         }
     }
 }
